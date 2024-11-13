7e648dc059728ad5afa65c127be7630079836cbb
Nabila Ayaba <nabilaayaba@gmail.com>
2024-11-13 10:33:46 +0300

    Create Index route for Hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -9,12 +9,23 @@ const router = express.Router()
 // ======= Protected Routes ==========
 router.use(verifyToken)
 
+// CREATE HOOT
 router.post('/', async (req, res) => {
     try {
         req.body.author = req.user._id
         const hoot = await Hoot.create(req.body)
         hoot._doc.author = req.user
         res.status(201).json(hoot)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// INDEX - GET ALL THE HOOTS
+router.get('/', async (req, res) => {
+    try {
+        const hoots = await Hoot.find({}).populate('author').sort({ createdAt: 'desc' })
+        res.status(200).json(hoots)
     } catch (error) {
         res.status(500).json(error)
     }
