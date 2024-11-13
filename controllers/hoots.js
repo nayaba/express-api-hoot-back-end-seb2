de272f0027aee7fd33a406a38b4c89176ae24080
Nabila Ayaba <nabilaayaba@gmail.com>
2024-11-13 11:04:20 +0300

    Create Comments Post router

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -86,4 +86,25 @@ router.delete('/:hootId', async (req, res) => {
 })
 
 
+// CREATE A COMMENT
+router.post('/:hootId/comments', async (req, res) => {
+    try {
+        req.body.author = req.user._id
+        const hoot = await Hoot.findById(req.params.hootId)
+        hoot.comments.push(req.body)
+        await hoot.save()
+
+        const newComment = hoot.comments[hoot.comments.length - 1]
+
+        newComment._doc.author = req.user
+
+        res.status(201).json(newComment)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// 
+
+
 module.exports = router
