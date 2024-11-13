be76216bce2e95d81eb8da4c91ac7a980635fea9
Nabila Ayaba <nabilaayaba@gmail.com>
2024-11-13 10:37:39 +0300

    Create Show controller

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -31,4 +31,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+// SHOW - GET ONE HOOT
+router.get('/:hootId', async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate('author')
+        res.status(200).json(hoot)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
